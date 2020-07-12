3cedb8eefcea6c63109c165dea28ac27b98a2d0d
MatthiasHerdt <herdtle95@live.de>
2020-07-12 18:01:10 +0200

    - Info Fragment - MediaPlayer - Layouting

--- app/src/main/java/com/example/muf_projekt_herdtle_hoch_puglia/Fragments/Fragment1.java
+++ app/src/main/java/com/example/muf_projekt_herdtle_hoch_puglia/Fragments/Fragment1.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,12 +23,6 @@ import com.example.muf_projekt_herdtle_hoch_puglia.MainViewModel;
 import com.example.muf_projekt_herdtle_hoch_puglia.Data.Memory;
 import com.example.muf_projekt_herdtle_hoch_puglia.R;
 import com.example.muf_projekt_herdtle_hoch_puglia.Data.SensorData;
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.Description;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.example.muf_projekt_herdtle_hoch_puglia.ViewModel.SensorViewModel;
 
 
@@ -60,148 +55,64 @@ public class Fragment1 extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_1,container,false);
+
+
+        /*ImageView imageView=(ImageView) view.findViewById(R.id.startFragment);
+        imageView.setImageResource(R.drawable.ic_launcher_background);*/
+
+
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
-        //final NavController controller = Navigation.findNavController(view);
-
-        final TextView vendor = view.findViewById(R.id.vendor);
-        final TextView name = view.findViewById(R.id.name);
-        final TextView version = view.findViewById(R.id.version);
-        final TextView xyz = view.findViewById(R.id.xyz);
-
         final Button StartButton = view.findViewById(R.id.startButton);
-        final Button StopButton = view.findViewById(R.id.stopButton);
         final Button FeedbackButton = view.findViewById(R.id.feedbackButton);
+
+        final Button InfoButton = view.findViewById(R.id.infoButton);
+
 
         observer = null;
         datalist = new ArrayList<>();
         mainViewModel = new ViewModelProvider(this,
                 ViewModelProvider.AndroidViewModelFactory.getInstance(getActivity().getApplication())).get(MainViewModel.class);
 
-        LineChart lineChart = view.findViewById(R.id.LiveDataChart);
-        Description desc_x = new Description();
-        desc_x.setText("");
-        lineChart.setDescription(desc_x);
-        lineChart.setDrawGridBackground(false);
-
-        ArrayList<Entry> values1 = new ArrayList<Entry>();
-        ArrayList<Entry> values2 = new ArrayList<Entry>();
-        ArrayList<Entry> values3 = new ArrayList<Entry>();
-        ArrayList<ILineDataSet> all = new ArrayList<>();
-
-
-        //view.findViewById(R.id.startButton).setOnClickListener(new View.OnClickListener() {
-        // Line Chart for LiveData
-
-
-
-
-        //Click Listener Start here
+        //Buttons
 
         StartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getContext(), "Messung gestartet", Toast.LENGTH_SHORT).show();
-                if(observer == null){
-                    observer = (sensorData)->{
-                        vendor.setText("Vendor" + sensorData.getSensor().getVendor());
-                        name.setText("Name" + sensorData.getSensor().getName());
-                        version.setText("Version" + sensorData.getSensor().getVersion());
-                        xyz.setText("x:" + sensorData.getP1() + "y:" +sensorData.getP2() + "z:" + sensorData.getP3());
-
-                        Memory tempmemory = new Memory(dataname, count, sensorData.getP1(),sensorData.getP2(),sensorData.getP3(),System.currentTimeMillis());
-                        datalist.add(tempmemory);
-                        // kann weg...
-                        //Log.d(TAG,"on Create: Daten: "+ datalist.get(count).getX());
-                        count=count+1;
-                        // eingabe in die Datenbank
-                        sensorViewModel.setSensor(tempmemory);
-
-
-                        xyz.setText(
-                                "x:" + sensorData.getP1() + "y:" +sensorData.getP2() + "z:" + sensorData.getP3());
-
-
-                        //datalist.add(new Memory(count, sensorData.getP1(),sensorData.getP2(),sensorData.getP3(),System.currentTimeMillis()));
-                        values1.add(new Entry(count,sensorData.getP1()));
-                        values2.add(new Entry(count, sensorData.getP2()));
-                        values3.add(new Entry(count, sensorData.getP3()));
-
-                        LineDataSet lineDataSetX = new LineDataSet(values1,"Data Set X");
-                        lineDataSetX.setColor(Color.GREEN);
-                        lineDataSetX.setDrawCircles(false);
-                        lineDataSetX.setDrawCircleHole(false);
-                        lineDataSetX.setDrawValues(false);
-
-                        LineDataSet lineDataSetY = new LineDataSet(values2,"Data Set Y");
-                        lineDataSetY.setColor(Color.BLUE);
-                        lineDataSetY.setDrawCircles(false);
-                        lineDataSetY.setDrawCircleHole(false);
-                        lineDataSetY.setDrawValues(false);
-
-                        LineDataSet lineDataSetZ = new LineDataSet(values3,"Data Set Z");
-                        lineDataSetZ.setColor(Color.BLACK);
-                        lineDataSetZ.setDrawCircles(false);
-                        lineDataSetZ.setDrawCircleHole(false);
-                        lineDataSetZ.setDrawValues(false);
-
-                        LineData data1 = new LineData(lineDataSetX,lineDataSetY,lineDataSetZ);
-
-                        lineChart.setData(data1);
-                        lineChart.invalidate();
-
-
-
-
-                        //  Entscheiden ob alle Senoren oder nur einer
-
-                        if(sensorData.getP1() > 2){
-
-                            Toast.makeText(getContext(), "WARNING! Beschleunigung zu Hoch",Toast.LENGTH_SHORT).show();
-                        }
-
-                        count = count + 1;
-
-                    };
-
-                    mainViewModel.sensorData.observe(getViewLifecycleOwner(),observer);
-                }
+                Navigation.findNavController(view).navigate(R.id.action_startFragment_to_feedbackfragement);
 
             }
 
         });
 
-        StopButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(), "Messung gestoppt", Toast.LENGTH_SHORT).show();
-                mainViewModel.sensorData.removeObserver(observer);
-                observer = null;
-                xyz.setText("Sie haben die Messung gestoppt." );
-                count = 0;
-                //datalist.clear();
-                values1.clear();
-                values2.clear();
-                values3.clear();
-                }
-        });
-
-
         FeedbackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getContext(), "Messung gestoppt", Toast.LENGTH_SHORT).show();
-                mainViewModel.sensorData.removeObserver(observer);
+                                mainViewModel.sensorData.removeObserver(observer);
                 observer = null;
-                xyz.setText("Sie haben die Messung gestoppt." );
+
                 count = 0;
                 //datalist.clear();
 
                 Navigation.findNavController(view).navigate(R.id.action_startFragment_to_zweitesFragment);
+            }
+        });
+
+        InfoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainViewModel.sensorData.removeObserver(observer);
+                observer = null;
+
+                count = 0;
+                //datalist.clear();
+
+                Navigation.findNavController(view).navigate(R.id.action_startFragment_to_infoFragment);
             }
         });
 
--- app/src/main/java/com/example/muf_projekt_herdtle_hoch_puglia/Fragments/Fragment2.java
+++ app/src/main/java/com/example/muf_projekt_herdtle_hoch_puglia/Fragments/Fragment2.java
@@ -1,24 +1,42 @@
 package com.example.muf_projekt_herdtle_hoch_puglia.Fragments;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.speech.tts.TextToSpeech;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import com.example.muf_projekt_herdtle_hoch_puglia.MediaService;
 import com.example.muf_projekt_herdtle_hoch_puglia.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.UUID;
+
+import static android.content.Context.BIND_AUTO_CREATE;
 
 public class Fragment2 extends Fragment {
+
+    private Button ding;
+    private MediaServiceConenction mediaServiceConnection = null;
+    private MediaService.MediaBinder mediaBinder;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_2,container,false);
+        View view = inflater.inflate(R.layout.fragment_2, container, false);
         return view;
     }
 
@@ -26,6 +44,7 @@ public class Fragment2 extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
         final Button HomeBildschrim = view.findViewById(R.id.BackToHome);
+        final Button Special = view.findViewById(R.id.specialButton);
 
         HomeBildschrim.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,5 +53,55 @@ public class Fragment2 extends Fragment {
                 Navigation.findNavController(view).navigate(R.id.action_zweitesFragment_to_startFragment);
             }
         });
+
+        Special.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(mediaBinder == null) return;
+                mediaBinder.play(R.raw.dings);
+            }
+        });
+
+    }
+
+
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mediaServiceConnection == null) {
+            getActivity().bindService(
+                    new Intent(getContext(), MediaService.class),
+                    mediaServiceConnection = new MediaServiceConenction(),
+                    Context.BIND_AUTO_CREATE
+            );
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mediaServiceConnection != null) {
+            getActivity().unbindService(mediaServiceConnection);
+            mediaServiceConnection = null;
+        }
+    }
+
+    private final class MediaServiceConenction implements ServiceConnection {
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mediaBinder = (MediaService.MediaBinder) service;
+
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mediaBinder = null;
+
+        }
     }
 }
